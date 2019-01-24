45f41f2eb11563cd791e55093185d622de703304
Harper Bellows <harper.bellows@gmail.com>
2019-01-24 09:52:04 -0700

    Add first crack at fetch call to forecast endpoint and returning current forecast summary

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,29 @@
 // This file is in the entry point in your webpack config.
+
+// 1. Weather for a location:
+// - GET /api/v1/forecast?location=denver,co
+// 2. Account Creation:
+// - POST /api/v1/users
+// 3. Login/Logout
+// - POST /api/v1/sessions
+// 4. Favoriting Locations
+//  - POST /api/v1/favorites
+// 5. Listing Favorite Locations
+//  - GET /api/v1/favorites?api_key={'blahblah'}
+// 6. Removing Favorite Locations
+//  - DELETE /api/v1/favorites
+
+function getWeather(location) {
+  var location = 'denver,co'
+  var url = `https://rain-or-shine-1.herokuapp.com/api/v1/forecast?location=${location}`
+  fetch(url)
+    .then(response) => response.json())
+    .then((response) => {
+      weatherData = response
+      return this.displayWeatherDetails(response)
+    }):
+}
+
+function displayWeatherDetails(response) {
+  document.getElementById("current-summary").innerHTML = (response.data.attributes.current_summary)
+}
