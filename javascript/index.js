7cd69cb62c65bd917b1a3e21fdd1df3275154cac
Harper Bellows <harper.bellows@gmail.com>
2019-01-25 06:19:49 -0700

    Add completed iteration zero for issue 11 -> Data coming back from API

--- javascript/index.js
+++ javascript/index.js
@@ -2,30 +2,31 @@
 
 // 1. Weather for a location:
 // - GET /api/v1/forecast?location=denver,co
-// 2. Account Creation:
-// - POST /api/v1/users
-// 3. Login/Logout
-// - POST /api/v1/sessions
-// 4. Favoriting Locations
+// 2. Favoriting Locations
 //  - POST /api/v1/favorites
-// 5. Listing Favorite Locations
+// 3. Listing Favorite Locations
 //  - GET /api/v1/favorites?api_key={'blahblah'}
-// 6. Removing Favorite Locations
+// 4. Removing Favorite Locations
 //  - DELETE /api/v1/favorites
+// 5. Account Creation:
+// - POST /api/v1/users
+// 6. Login/Logout
+// - POST /api/v1/sessions
 
 const productionUrl = 'https://rain-or-shine-1.herokuapp.com';
 
 function getWeather(location) {
-  // debugger
   var location = location
   var url = `${productionUrl}/api/v1/forecast?location=${location}`
   fetch(url)
   .then((response) => response.json())
   .then((res) => {
-    return this.displayWeatherDetails(res)
-  // .catch(error => console.log({error}))
-  });
-}
+    displayWeatherDetails(res);
+  })
+  .catch(error => {
+    console.log(error)
+    });
+};
 
 $('#location-search').on('click', function() {
   var location = $('#location').val();
@@ -35,11 +36,3 @@ $('#location-search').on('click', function() {
 function displayWeatherDetails(response) {
   document.getElementById("current-summary-container").innerHTML = (response.data.attributes.current_forecast)
 }
-
-
-// const fetchDiscussions = () => {
-//   fetch('/api/v1/discussions')
-//   .then((response) => response.json())
-//   .then((rawDiscussions) => cleanDiscussions(rawDiscussions))
-//   .catch((error) => console.error({ error }));
-// }
