4f75987ab92749f19466e3646d76b47b122f88eb
Harper Bellows <harper.bellows@gmail.com>
2019-01-25 18:37:42 -0700

    add updated main.bundle.js file after running npm run build

--- main.bundle.js
+++ main.bundle.js
@@ -50,47 +50,41 @@
 
 	// 1. Weather for a location:
 	// - GET /api/v1/forecast?location=denver,co
-	// 2. Account Creation:
-	// - POST /api/v1/users
-	// 3. Login/Logout
-	// - POST /api/v1/sessions
-	// 4. Favoriting Locations
+	// 2. Favoriting Locations
 	//  - POST /api/v1/favorites
-	// 5. Listing Favorite Locations
+	// 3. Listing Favorite Locations
 	//  - GET /api/v1/favorites?api_key={'blahblah'}
-	// 6. Removing Favorite Locations
+	// 4. Removing Favorite Locations
 	//  - DELETE /api/v1/favorites
+	// 5. Account Creation:
+	// - POST /api/v1/users
+	// 6. Login/Logout
+	// - POST /api/v1/sessions
 
 	var productionUrl = 'https://rain-or-shine-1.herokuapp.com';
 
-	function getWeather(location) {
-	  debugger;
-	  var location = location;
+	var getCurrentWeather = function getCurrentWeather(location) {
 	  var url = productionUrl + '/api/v1/forecast?location=' + location;
 	  fetch(url).then(function (response) {
 	    return response.json();
-	  }).then(function (weatherResponse) {
-	    return displayWeatherDetails(weatherResponse).catch(function (error) {
-	      return console.log({ error: error });
-	    });
+	  }).then(function (res) {
+	    displayCurrentWeatherSummary(res);
+	  }).catch(function (error) {
+	    console.log(error);
 	  });
-	}
+	};
 
 	$('#location-search').on('click', function () {
 	  var location = $('#location').val();
-	  getWeather(location);
+	  getCurrentWeather(location);
 	});
 
-	function displayWeatherDetails(response) {
-	  document.getElementById("current-summary-container").innerHTML = response.data.attributes.current_forecast;
-	}
-
-	// const fetchDiscussions = () => {
-	//   fetch('/api/v1/discussions')
-	//   .then((response) => response.json())
-	//   .then((rawDiscussions) => cleanDiscussions(rawDiscussions))
-	//   .catch((error) => console.error({ error }));
-	// }
+	var displayCurrentWeatherSummary = function displayCurrentWeatherSummary(response) {
+	  // document.getElementById("current-summary-container").innerHTML = (response.data.attributes.current_forecast.time)
+	  $("#current-summary").html('');
+	  $('#current-summary').append('\n    <div class="summary-left">\n      <h5>\n        <span class="currently-location">' + response.data.id + '</span>\n      </h5>\n      <h5>\n        <span id="currently-summary">' + response.data.attributes.current_forecast.summary + '</span>\n      </h5>\n      <h5>\n        <span class="currently-time">' + response.data.attributes.current_forecast.time + '</span>\n      </h5>\n      <h5>\n        <span id="currently-temperature">Now ' + response.data.attributes.current_forecast.temp + '</span>&deg;\n      </h5>\n      <h5>\n        <span id="currently-apparent-temperature">Feels Like ' + response.data.attributes.current_forecast.feels_like + '</span>&deg;\n      </h5>\n      <h5>\n        <span id="currently-humidity">Humdiity ' + response.data.attributes.current_forecast.humidity + '%</span>\n      </h5>\n      <h5>\n        <span id="currently-uvIndex">UV Index ' + response.data.attributes.current_forecast.uv_index + '</span>\n      </h5>\n    </div>\n  ');
+	  $('#current-summary').css('display', 'inherit');
+	};
 
 /***/ })
 /******/ ]);
